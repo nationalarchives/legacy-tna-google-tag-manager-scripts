e16e52e831bff2b17f4822c092c3329b7d630d22
Punal Chotrani <punalsc@gmail.com>
2018-05-25 14:35:28 +0100

    discovery details content group group refactored script

---
+++ src/discoveryDetailsContentGroup.js
@@ -0,0 +1,35 @@
+/**
+ * ------------  THE NATIONAL ARCHIVES  -----------------
+ * JS - getting the content group in Discovery Details page for Google Tag Manager
+ * Developer: Punal Chotrani
+ **/
+
+function gtmLoadEvent(func){
+    let oldOnLoad = window.onload;
+    if (typeof window.onload !== 'function') {
+        window.onload = func;
+    } else {
+        window.onload = () => {
+            oldOnLoad();
+            func();
+        };
+    }
+}
+gtmLoadEvent(() => {
+    let metaTag = document.querySelector('meta[name=WT\\.cg_n]'),
+        $metaDataContent = metaTag.getAttribute("content"),
+        $metaDataName = metaTag.getAttribute('name'),
+        $discovery = 'Discovery';
+
+    if ( $metaDataName === 'WT.cg_n') {
+
+        /*Pushing stuff to Google's Data Layer*/
+        dataLayer.push({
+            'event'         : $discovery,
+            'eventCategory' : $discovery,
+            'eventAction'   : $metaDataName,
+            'eventLabel'    : $metaDataContent,
+            'ContentGroup' : $metaDataContent
+        });
+    }
+});
