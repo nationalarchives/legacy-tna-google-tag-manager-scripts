ff3792e104dc71101d59c852e648f1137cff5605
Punal Chotrani <punalsc@gmail.com>
2018-05-25 17:29:25 +0100

    refactoring code for testing

--- src/discoveryDetailsContentGroup.js
+++ src/discoveryDetailsContentGroup.js
@@ -3,33 +3,24 @@
  * JS - getting the content group in Discovery Details page for Google Tag Manager
  * Developer: Punal Chotrani
  **/
-
-let gtmLoadEvent = (func) => {
-    let oldOnLoad = window.onload;
-    if (typeof window.onload !== 'function') {
-        window.onload = func;
-    } else {
-        window.onload = () => {
-            oldOnLoad();
-            func();
-        };
-    }
-};
-gtmLoadEvent(() => {
+let discoveryDetailsContentGroup = () => {
     let metaTag = document.querySelector('meta[name=WT\\.cg_n]'),
         $metaDataContent = metaTag.getAttribute("content"),
         $metaDataName = metaTag.getAttribute('name'),
         $discovery = 'Discovery';
 
-    if ( $metaDataName === 'WT.cg_n') {
-
-        /*Pushing stuff to Google's Data Layer*/
-        dataLayer.push({
-            'event'         : $discovery,
-            'eventCategory' : $discovery,
-            'eventAction'   : $metaDataName,
-            'eventLabel'    : $metaDataContent,
-            'ContentGroup' : $metaDataContent
-        });
+    if ($metaDataName === 'WT.cg_n' ) {
+        return () => {
+            window.dataLayer.push({
+                'event'         : $discovery,
+                'eventCategory' : $discovery,
+                'eventAction'   : $metaDataName,
+                'eventLabel'    : $metaDataContent,
+                'ContentGroup' : $metaDataContent
+            });
+        }
     }
-});
+};
+
+discoveryDetailsContentGroup();
+
