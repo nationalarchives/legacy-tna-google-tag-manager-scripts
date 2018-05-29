0ef3e829a627a7901b8f90b1dd764b14ffa8267d
Punal Chotrani <punalsc@gmail.com>
2018-05-29 15:09:45 +0100

    refactored delivery option script

---
+++ src/deliveryOptionChosen.js
@@ -0,0 +1,23 @@
+/**
+ * ------------  THE NATIONAL ARCHIVES  -----------------
+ * JS - getting the data attributes when the user clicks on a delivery option in Discovery Details page for Google Tag Manager
+ * Developer: Punal Chotrani
+ **/
+
+let deliveryOptionButton = document.querySelector('.order-option-wrapper a');
+
+deliveryOptionButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    let $discovery = 'Discovery',
+        $eventAction = 'Delivery option chosen',
+        $eventLabel = this.dataset.webtrendsCall;
+
+    return () => {
+        window.dataLayer.push({
+            'event'         : $discovery,
+            'eventCategory' : $discovery,
+            'eventAction'   : $eventAction,
+            'eventLabel'    : $eventLabel
+        });
+    };
+}, false);
