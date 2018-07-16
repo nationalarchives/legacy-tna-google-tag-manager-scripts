56b1c893152c92384a47c253022f5e949b2d66c6
Punal Chotrani <punalsc@gmail.com>
2018-07-16 16:51:39 +0100

    more tests

--- src/bannerClick.js
+++ src/bannerClick.js
@@ -1,3 +1,4 @@
 import {promoObjFunc} from './modules/promoObj';
 
-console.log(promoObjFunc('promotionClick', 'bts', 'Image viewer banner', 'Banner', 'On watermarked image'));
+//console.log(promoObjFunc('promotionClick', 'bts', 'Image viewer banner', 'Banner', 'On watermarked image'));
+console.log(promoObjFunc('promotionClick', 'bts', 'Image viewer banner', 'Banner', 'On watermarked image').ecommerce.promoClick.promotions[0].id);
