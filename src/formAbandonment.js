01385b319abf3c8769b85498cbaabf868968ca94
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2018-07-17 16:10:14 +0100

    In progress

--- src/formAbandonment.js
+++ src/formAbandonment.js
@@ -1,3 +1,10 @@
+/**
+ * ------------  THE NATIONAL ARCHIVES  -----------------
+ * JS - Form abandonment on contact forms
+ * Developer: Mihai Diaconita
+ **/
+
 import { getFormIDByClassName } from './modules/getFormIDByClassName';
 
-console.log(getFormIDByClassName('form-abandonment'));
+// This grabs the ID of the form
+console.log(`${getFormIDByClassName('form-abandonment')}`);
---
+++ src/modules/checkDataLayerFormSubmit.js
@@ -0,0 +1,18 @@
+/**
+ * Check if the form was submitted
+ * */
+let checkDataLayerFormSubmit = (dataLayer) => {
+    if (Array.isArray(dataLayer)) {
+        let i = dataLayer.length - 1;
+        while (i > -1) {
+            if (dataLayer[i]['event'] === 'gtm.formSubmit') {
+                return true;
+            }
+            i--;
+        }
+        return true;
+    }
+    return false;
+}
+
+module.exports.checkDataLayerFormSubmit = checkDataLayerFormSubmit;
