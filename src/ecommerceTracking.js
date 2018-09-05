231f411c68a16cca916c7d4eb9b34e2747b9e468
JamesWChan <jwchan1993@gmail.com>
2018-09-05 16:19:42 +0100

    Initial commit

--- src/ecommerceTracking.js
+++ src/ecommerceTracking.js
@@ -19,3 +19,5 @@ window.dataLayer.push(buildEcommerceObj(
     extractMetaTagContent('WT\\.tx_total', 'Meta tag not available'),
     buildProductsObjArray(extractProductName(productsArray), productsArray, pricesArray, categoriesArray, calculateQuantity(productsArray))
 ));
+
+//Hello
