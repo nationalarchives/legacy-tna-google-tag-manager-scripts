8c72a4cfa7e4bf6ad22e092b0df946744538e853
Mihai Diaconita <michaiel.webdesigner@gmail.com>
2018-09-06 12:32:33 +0100

    Refactor Home Page script

---
+++ src/modules/pushInDataLayer.js
@@ -0,0 +1,12 @@
+/**
+ * ------------  THE NATIONAL ARCHIVES  -----------------
+ * JS - Push In DataLayer component
+ * Developer: Mihai Diaconita
+ **/
+
+export const pushInDataLayer = (obj) => {
+    let wd = window.dataLayer || [];
+    (!!obj || typeof obj === 'object') ? wd.push(obj) : '';
+
+    return obj;
+};
