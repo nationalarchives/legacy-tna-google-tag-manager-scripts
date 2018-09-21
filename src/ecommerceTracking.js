fe04e7e57d41f74bcae6ea3daebfbdc87ac446c6
JamesWChan <jwchan1993@gmail.com>
2018-09-21 13:10:08 +0100

    Added condition around pushing ecommerce object to data layer

--- src/ecommerceTracking.js
+++ src/ecommerceTracking.js
@@ -4,6 +4,9 @@ import{extractMetaTagContent} from './modules/extractMetaTagContent';
 import{extractProductName} from './modules/extractProductName';
 import{buildEcommerceObj} from './modules/buildEcommerceObj';
 
+//Extracts the step of the process
+let step = document.querySelector('meta[name = WT\\.si_p ]').content;
+
 //Creates an array from the 'content' attribute, using the semi-colon as a delimiter
 let categoriesArray = extractMetaTagContent('WT\\.pn_fa', 'Categories meta tag not available').split(';');
 let productsArray = extractMetaTagContent('WT\\.pn_sku', 'Product meta tag not available').split(';');
@@ -12,10 +15,13 @@ let pricesArray = extractMetaTagContent('WT\\.tx_s', 'Prices meta tag not availa
 //dataLayer is equal to the pre-existing GTM data layer, else is initialised to an empty array
 window.dataLayer = window.dataLayer || [];
 
-window.dataLayer.push(buildEcommerceObj(
-    extractMetaTagContent('WT\\.si_p', 'Meta tag not available'),
-    extractMetaTagContent('WT\\.tx_id', 'Meta tag not available'),
-    extractMetaTagContent('WT\\.si_n', 'Meta tag not available'),
-    extractMetaTagContent('WT\\.tx_total', 'Meta tag not available'),
-    buildProductsObjArray(extractProductName(productsArray), productsArray, pricesArray, categoriesArray, calculateQuantity(productsArray))
-));
+//Verifies the step and pushes the ecommerce object to the data layer if step = Step 4
+if(step === 'Step 4') {
+    window.dataLayer.push(buildEcommerceObj(
+        extractMetaTagContent('WT\\.si_p', 'Meta tag not available'),
+        extractMetaTagContent('WT\\.tx_id', 'Meta tag not available'),
+        extractMetaTagContent('WT\\.si_n', 'Meta tag not available'),
+        extractMetaTagContent('WT\\.tx_total', 'Meta tag not available'),
+        buildProductsObjArray(extractProductName(productsArray), productsArray, pricesArray, categoriesArray, calculateQuantity(productsArray))
+    ));
+}
