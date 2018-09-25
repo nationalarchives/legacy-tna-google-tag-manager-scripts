94f5bc3cb04668e160fe4317911d191ffeb5f243
Punal Chotrani <punalsc@gmail.com>
2018-09-25 18:46:30 +0100

    get image label string

---
+++ src/modules/imageViewer/imageControlSchema.js
@@ -0,0 +1,8 @@
+import {getEventLabel} from './eventLabel';
+
+export const controlObj = {
+    'event': 'Discovery image viewer',
+    'eventCategory': 'Discovery',
+    'eventAction': 'Image viewer functions',
+    'eventLabel': getEventLabel
+};
