136bb3cd02262c24208a7ce49ea205dca4a69c8e
Punal Chotrani <punalsc@gmail.com>
2018-09-26 18:46:14 +0100

    get image page logic

--- src/imageViewer.js
+++ src/imageViewer.js
@@ -1,9 +1,15 @@
 import {controlObj} from './modules/imageViewer/imageControlSchema';
 
-export const imageViewer = document.getElementById('imageViewer');
-imageViewer.addEventListener('click', function (e) {
+const imageViewer = document.getElementById('imageViewer');
+imageViewer.addEventListener('click', (e) => {
     if ( e.target.tagName === 'SPAN' || e.target.tagName === 'A' ) {
         console.log(controlObj());
         e.preventDefault();
     }
+}, false);
+
+
+const selectOption = imageViewer.querySelector('select option');
+selectOption.addEventListener('select', function() {
+    console.log(this.value);
 }, false);
