e59b27840d1e274ce09393b0defe9395e3b58fa7
Punal Chotrani <punalsc@gmail.com>
2018-09-28 11:59:13 +0100

    cleaned up logic

--- src/modules/imageViewer/eventLabel.js
+++ src/modules/imageViewer/eventLabel.js
@@ -1,12 +1,12 @@
+import {imageViewer} from '../../imageViewer';
+
 export const getEventLabel = (title) => {
-    let pageNumber = imageViewer.querySelector('select option[selected="selected"]').text;
-    if (event.target.hasChildNodes('img')) {
-        if ( event.target.getAttribute(title) === 'View full size image' ) {
-            return 'Thumbnail page ' + pageNumber;
+    let pageNumber = imageViewer.querySelector('select option[selected="selected"]');
+    if ( event.target.getAttribute(title) === 'View full size image' ) {
+        if( pageNumber ) {
+            return `Thumbnail image ${pageNumber.text}`;
         }
-        return event.target.getAttribute(title);
-    } else if (event.target.childNodes.length >= 1) {
-        return event.target.innerHTML;
+        return 'Thumbnail image - No page';
     } else {
         return event.target.getAttribute(title);
     }
