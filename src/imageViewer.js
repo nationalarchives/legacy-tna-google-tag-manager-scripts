7d250bb446dbb8085016583924e6371ac320d866
Punal Chotrani <punalsc@gmail.com>
2018-09-28 15:22:02 +0100

    more fixes to script

--- src/imageViewer.js
+++ src/imageViewer.js
@@ -2,9 +2,6 @@ import {controlObj} from './modules/imageViewer/imageControlSchema';
 export const imageViewer = document.getElementById('imageViewer');
 
 imageViewer && imageViewer.addEventListener('click', (e) => {
-    if (!imageViewer) {
-        return;
-    }
     if ( e.target.nodeName === 'A' || e.target.matches('.control-buttons span')  ) {
         console.log(controlObj());
         e.preventDefault();
