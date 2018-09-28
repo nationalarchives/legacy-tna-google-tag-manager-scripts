c51a71950782b342793caa853bf3c42a5b504042
Punal Chotrani <punalsc@gmail.com>
2018-09-28 11:59:13 +0100

    cleaned up logic

--- src/imageViewer.js
+++ src/imageViewer.js
@@ -1,9 +1,9 @@
 import {controlObj} from './modules/imageViewer/imageControlSchema';
-const imageViewer = document.getElementById('imageViewer');
+export const imageViewer = document.getElementById('imageViewer');
 
 
 imageViewer.addEventListener('click', (e) => {
-    if ( e.target.tagName === 'SPAN' || e.target.tagName === 'A' ) {
+    if ( e.target.tagName === 'A' || e.target === imageViewer.querySelector('.control-buttons span') ) {
         console.log(controlObj());
         e.preventDefault();
     }
