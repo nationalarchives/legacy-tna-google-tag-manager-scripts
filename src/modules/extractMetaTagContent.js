28670a7ca4253679468ab2a165322acd7e52b6de
JamesWChan <jwchan1993@gmail.com>
2018-11-14 11:07:07 +0000

    Added splitToArray helper function and modified extractMetaTagContent module

--- src/modules/extractMetaTagContent.js
+++ src/modules/extractMetaTagContent.js
@@ -3,13 +3,11 @@ param: String, String
 return: String
 */
 
-export const extractMetaTagContent = (metaTagName) => {
-    let domElement = (typeof metaTagName !== 'string') ? null : document.querySelector(`meta[name = ${metaTagName} ]`);
-
-    //Returns a custom message if domElement is not a String or if domElement/domElement content attribute doesn't exist
-    //Else returns meta tag content
-    if(!domElement || !domElement.getAttribute('content')){
-        return null;
+export const extractMetaTagContent = (metaTagName = null) => {
+    let domElement = document.querySelector(`meta[name = ${metaTagName} ]`);
+    if (domElement) {
+        return domElement.getAttribute('content');
     }
-    return domElement.getAttribute('content');
+    return null;
 };
+    
