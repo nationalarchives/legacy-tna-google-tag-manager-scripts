87b3e50069107ac8397545889329ce8975c8ad1d
JamesWChan <jwchan1993@gmail.com>
2018-12-12 12:29:53 +0000

    Added extra condition in removeNullValues to remove empty objects

--- src/modules/removeNullValues.js
+++ src/modules/removeNullValues.js
@@ -3,9 +3,15 @@ export let removeNullValues = (obj) => {
         if (obj[key] === Object(obj[key])) {
             removeNullValues(obj[key]);
         }
-        else if(obj[key] === null || obj[key] === undefined){
+        else if(obj[key] === null || obj[key] === undefined) {
             delete obj[key];
         }
+        if(obj[key] === Object(obj[key])){
+            if(Object.keys(obj[key]).length === 0){
+                delete obj[key];
+            }
+        }
     });
+
     return obj;
 };
