944a4c042ca498db9479ccd83d46d323a4d8fd3c
Punal Chotrani <punalsc@gmail.com>
2018-12-14 16:29:02 +0000

    removed watchAll flag

--- src/imageViewer.js
+++ src/imageViewer.js
@@ -1,18 +1,17 @@
 import {controlObj} from './modules/imageViewer/imageControlSchema';
 import {getEventLabel} from './modules/imageViewer/eventLabel';
-//import {pushInDataLayer} from './modules/pushInDataLayer';
+import {pushInDataLayer} from './modules/pushInDataLayer';
 
 export const imageViewer = document.getElementById('imageViewer');
 
-export const handleEvent = e => {
+const handleEvent = e => {
     if (    e.target.nodeName === 'A' ||
             e.target.nodeName === 'SELECT' ||
             e.target.matches('#thumbPanel img') ||
             e.target.matches('.control-buttons span')
     ) {
-        //pushInDataLayer(controlObj('Discovery image viewer', 'Discovery', 'Image viewer functions', getEventLabel('title')));
-        console.log(controlObj('Discovery image viewer', 'Discovery', 'Image viewer functions', getEventLabel('title')));
-        e.preventDefault();
+        pushInDataLayer(controlObj('Discovery image viewer', 'Discovery', 'Image viewer functions', getEventLabel('title')));
+        //e.preventDefault();
     }
 };
 imageViewer && imageViewer.addEventListener('click', handleEvent, false);
--- test/imageControlSchema.test.js
+++ test/imageControlSchema.test.js
@@ -1,6 +1,6 @@
 import {controlObj} from '../src/modules/imageViewer/imageControlSchema';
 
-describe('Should have the defined property & type of arguments', ()=>{
+describe('Should have the defined property & type of arguments', () => {
     it('Should have the defined property', () => {
         expect(controlObj('arg1', 'arg2', 'arg3', 'arg4')).toHaveProperty('event');
         expect(controlObj('arg1', 'arg2', 'arg3','arg4')).toHaveProperty('eventCategory');
@@ -10,7 +10,7 @@ describe('Should have the defined property & type of arguments', ()=>{
     it('Should test Defined', ()=>{
         expect(controlObj('arg1', 'arg2', 'arg3', '')).toBeDefined();
     });
-    it('Should test arguments if not present', ()=>{
+    it('Should test arguments if not present', () => {
         expect(controlObj()).toEqual({
             'event': 'no event',
             'eventCategory': 'no category',
