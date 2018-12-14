461997e0b96e48c0d4bf7daf1e7e2178854473ea
Punal Chotrani <punalsc@gmail.com>
2018-12-14 16:04:51 +0000

    Testing the return object

--- src/modules/imageViewer/imageControlSchema.js
+++ src/modules/imageViewer/imageControlSchema.js
@@ -1,16 +1,8 @@
 export const controlObj = (arg1, arg2, arg3, arg4) => {
-    if (
-        typeof arg1 !== 'string' ||
-        typeof arg2 !== 'string' ||
-        typeof arg3 !== 'string' ||
-        typeof arg4 !== 'function'
-    ) {
-        return;
-    }
     return {
-        'event': arg1,
-        'eventCategory': arg2,
-        'eventAction': arg3,
-        'eventLabel': arg4
+        'event': arg1 || 'no event',
+        'eventCategory': arg2 || 'no category',
+        'eventAction': arg3 || 'no event action',
+        'eventLabel': arg4 || 'no event label'
     };
 };
--- test/imageControlSchema.test.js
+++ test/imageControlSchema.test.js
@@ -2,13 +2,20 @@ import {controlObj} from '../src/modules/imageViewer/imageControlSchema';
 
 describe('Should have the defined property & type of arguments', ()=>{
     it('Should have the defined property', () => {
-        let argFunc = () => 'This is a function'
-        expect(controlObj('arg1', 'arg2', 'arg3', argFunc)).toHaveProperty('event');
-        expect(controlObj('arg1', 'arg2', 'arg3', argFunc)).toHaveProperty('eventCategory');
-        expect(controlObj('arg1', 'arg2', 'arg3', argFunc)).toHaveProperty('eventAction');
-        expect(controlObj('arg1', 'arg2', 'arg3', argFunc)).toHaveProperty('eventLabel');
+        expect(controlObj('arg1', 'arg2', 'arg3', 'arg4')).toHaveProperty('event');
+        expect(controlObj('arg1', 'arg2', 'arg3','arg4')).toHaveProperty('eventCategory');
+        expect(controlObj('arg1', 'arg2', 'arg3', 'arg4')).toHaveProperty('eventAction');
+        expect(controlObj('arg1', 'arg2', 'arg3', 'arg4')).toHaveProperty('eventLabel');
     });
-    it('Should test the argument type', ()=>{
-
+    it('Should test Defined', ()=>{
+        expect(controlObj('arg1', 'arg2', 'arg3', '')).toBeDefined();
+    });
+    it('Should test arguments if not present', ()=>{
+        expect(controlObj()).toEqual({
+            'event': 'no event',
+            'eventCategory': 'no category',
+            'eventAction': 'no event action',
+            'eventLabel': 'no event label'
+        });
     });
 });
