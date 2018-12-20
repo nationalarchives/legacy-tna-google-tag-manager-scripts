a428dfb59f3c2234661247e0c07baeca52ab3009
JamesWChan <jwchan1993@gmail.com>
2018-12-20 16:52:11 +0000

    Added further tests

--- src/modules/filterTracking/cleanLabelName.js
+++ src/modules/filterTracking/cleanLabelName.js
@@ -1,7 +1,7 @@
 // Extracts the filter label so it is readable
 export let cleanLabelName = (label) => {
     if(typeof label === 'string') {
-        label = label.split(' (');
+        label = label.split('(');
         return label[0];
     }
     return 'The parameters are of the incorrect data type.';
--- test/filterTracking.test.js
+++ test/filterTracking.test.js
@@ -1,10 +1,45 @@
-import {filterTrackingDOM} from './filterTrackingDOM';
 import {buildFilterTrackingObj} from '../src/modules/filterTracking/buildFilterTrackingObj';
 import {cleanLabelName} from '../src/modules/filterTracking/cleanLabelName';
 import {verifyFilter} from '../src/modules/filterTracking/verifyFilter';
+import {addListenersToFilters} from '../src/modules/filterTracking/addListenersToFilters';
 
 document.body.innerHTML =
-    '<label for="C10092">Army<span>(6,530,838)</span></label>';
+    '<label for="C10092">Army<span>(6,530,838)</span></label>'+
+    '<li id="subjects">'+
+        '<form action="/results/r" method="get"><input name="_q" type="hidden" value="*" /><input name="_hb" type="hidden" value="tna" />'+
+        '<h3><a href="#" class="filter-toggler" id="subject-filters" role="tab">Subjects</a></h3>'+
+            '<div class="filter-togglee" aria-labelledby="subject-filters" role="tabpanel">'+
+                '<ul>'+
+                    '<li>'+
+                        '<input type="checkbox" name="_tsj" value="C10065" id="C10065" />'+
+                        '<label for="C10065">'+
+                            'Armed Forces (General Administration)'+
+                            '<span>(9,392,340)</span>'+
+                        '</label>'+
+                    '</li>'+
+                    '<li>'+
+                        '<input type="checkbox" name="_tsj" value="C10092" id="C10092" />'+
+                        '<label for="C10092">'+
+                            'Army'+
+                            '<span>(6,530,838)</span>'+
+                        '</label>'+
+                    '</li>'+
+                    '<li>'+
+                        '<input type="checkbox" name="_tsj" value="C10075" id="C10075" checked="checked" />'+
+                        '<label for="C10075">'+
+                            'Operations, battles and campaigns'+
+                            '<span>(6,248,824)</span>'+
+                        '</label>'+
+                    '</li>'+
+                '</ul>'+
+            '</div>'+
+        '</form>'+
+    '</li>';
+
+let appliedSubjectFiltersArray = [];
+let armyFilter = document.querySelector('label[for="C10092"]');
+let subjectFilters = Array.from(document.querySelectorAll('#subjects ul li input'));
+let subjectLabels = Array.from(document.querySelectorAll('#subjects ul li label'));
 
 describe('Checks that the filter tracking object is built correctly', () => {
     it('Should return an object if correct parameters are received, else returns error message', () => {
@@ -27,11 +62,11 @@ describe('Checks that the filter tracking object is built correctly', () => {
 
 describe('Checks that the filter name is cleaned correctly', () => {
     it('Should return only the name (i.e. excludes search results statistic)', () => {
-        //let armyFilter = document.querySelector('label[for="C10092"]');
-        expect(cleanLabelName('Army (6,530,838)')).toBe('Army');
+
+        expect(cleanLabelName(armyFilter.textContent)).toBe('Army');
     });
     it('Should return a string if the correct parameters are received, else returns error message', () => {
-        expect(typeof cleanLabelName('Army (6,530,838)')).toBe('string');
+        expect(typeof cleanLabelName(armyFilter.textContent)).toBe('string');
         expect(cleanLabelName({})).toBe('The parameters are of the incorrect data type.');
     });
 });
@@ -46,5 +81,11 @@ describe('Checks that the verify filter returns the correct value', () => {
     });
     it('Should return subjectFilters when the refine subject button is passed', () => {
         expect(verifyFilter('Refine subjects', ['appliedDateFilters'], ['appliedSubjectFilters'])).toEqual(['appliedSubjectFilters']);
+    });
+});
+
+describe('Checks that selected filters are returned', () => {
+    it('Should return an empty array if no filters are selected', () => {
+        expect(addListenersToFilters([], [], [])).toEqual([]);
     });
 });
