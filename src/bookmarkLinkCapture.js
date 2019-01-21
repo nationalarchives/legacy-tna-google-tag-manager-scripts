932d9c78316bb9e1406dfc5dbc56e09bfcd244d5
Punal Chotrani <punalsc@gmail.com>
2019-01-21 12:21:49 +0000

    Added more tests

--- src/bookmarkLinkCapture.js
+++ src/bookmarkLinkCapture.js
@@ -4,24 +4,24 @@ import { capitalizeFirstLetter } from './modules/capitalizeFirstLetter';
 export const checkValue = ({ target }) => target.innerHTML.includes('result') ? capitalizeFirstLetter('search results bookmark') : capitalizeFirstLetter('catalogue description bookmark');
 
 export const renderObjFunc = (eventArg, catArg, actionArg, labelArg) => {
-    if (typeof eventArg !== 'string' && typeof catArg !== 'string' && typeof actionArg !== 'string' && typeof labelArg !== 'string') {
-        return null;
-    }
-    return {
-        'event': capitalizeFirstLetter(eventArg),
-        'eventCategory': capitalizeFirstLetter(catArg),
-        'eventAction': capitalizeFirstLetter(actionArg),
-        'eventLabel': labelArg
-    };
+	if (typeof eventArg !== 'string' && typeof catArg !== 'string' && typeof actionArg !== 'string' && typeof labelArg !== 'string') {
+		return null;
+	}
+	return {
+		'event': capitalizeFirstLetter(eventArg),
+		'eventCategory': capitalizeFirstLetter(catArg),
+		'eventAction': capitalizeFirstLetter(actionArg),
+		'eventLabel': labelArg
+	};
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('#bookmarks .bookmarkMain a');
+	const links = document.querySelectorAll('#bookmarks .bookmarkMain a');
 
-    links.forEach(link => {
-        link.addEventListener('click', e => {
-            e.preventDefault();
-            pushInDataLayer(renderObjFunc('discovery', 'discovery', 'bookmark link', checkValue(e)));
-        });
-    });
+	links.forEach(link => {
+		link.addEventListener('click', e => {
+			e.preventDefault();
+			pushInDataLayer(renderObjFunc('discovery', 'discovery', 'bookmark link', checkValue(e)));
+		});
+	});
 });
--- test/bookmarkLinkCapture.test.js
+++ test/bookmarkLinkCapture.test.js
@@ -42,14 +42,13 @@ describe('Simulate click event', () => {
 	it('Test click event', () => {
 		const evt = document.createEvent('MouseEvents');
 		evt.initEvent('click', false, true);
-		document.body.dispatchEvent(evt);
 		document.body.innerHTML = `<ul><li><a id="result" href="/result">http://localhost:81/results/r?_q=felix</a></li></ul>`;
 
 		const resultButton = document.getElementById('result');
 		resultButton.addEventListener('click', e => {
 			expect(checkValue(e)).toBe('Search results bookmark');
+			expect(checkValue(e)).not.toBe('Search results bookmarkss');
 		});
-		
 		resultButton.dispatchEvent(evt);
 	});
 });
