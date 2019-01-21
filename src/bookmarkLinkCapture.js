d851f37d31eaf7f8f089efd7270836af0089a756
Punal Chotrani <punalsc@gmail.com>
2019-01-21 13:43:16 +0000

    Updated checkvalue function to check href link

--- src/bookmarkLinkCapture.js
+++ src/bookmarkLinkCapture.js
@@ -1,27 +1,27 @@
 import { pushInDataLayer } from './modules/pushInDataLayer';
 import { capitalizeFirstLetter } from './modules/capitalizeFirstLetter';
 
-export const checkValue = ({ target }) => target.innerHTML.includes('result') ? capitalizeFirstLetter('search results bookmark') : capitalizeFirstLetter('catalogue description bookmark');
+export const checkValue = ({ target }) => target.href.includes('result') ? capitalizeFirstLetter('search results bookmark') : capitalizeFirstLetter('catalogue description bookmark');
 
 export const renderObjFunc = (eventArg, catArg, actionArg, labelArg) => {
-	if (typeof eventArg !== 'string' && typeof catArg !== 'string' && typeof actionArg !== 'string' && typeof labelArg !== 'string') {
-		return null;
-	}
-	return {
-		'event': capitalizeFirstLetter(eventArg),
-		'eventCategory': capitalizeFirstLetter(catArg),
-		'eventAction': capitalizeFirstLetter(actionArg),
-		'eventLabel': labelArg
-	};
+    if (typeof eventArg !== 'string' && typeof catArg !== 'string' && typeof actionArg !== 'string' && typeof labelArg !== 'string') {
+        return null;
+    }
+    return {
+        'event': capitalizeFirstLetter(eventArg),
+        'eventCategory': capitalizeFirstLetter(catArg),
+        'eventAction': capitalizeFirstLetter(actionArg),
+        'eventLabel': labelArg
+    };
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-	const links = document.querySelectorAll('#bookmarks .bookmarkMain a');
+    const links = document.querySelectorAll('#bookmarks .bookmarkMain a');
 
-	links.forEach(link => {
-		link.addEventListener('click', e => {
-			e.preventDefault();
-			pushInDataLayer(renderObjFunc('discovery', 'discovery', 'bookmark link', checkValue(e)));
-		});
-	});
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+            pushInDataLayer(renderObjFunc('discovery', 'discovery', 'bookmark link', checkValue(e)));
+        });
+    });
 });
--- test/bookmarkLinkCapture.test.js
+++ test/bookmarkLinkCapture.test.js
@@ -39,10 +39,10 @@ describe('Checking output values', () => {
 });
 
 describe('Simulate click event', () => {
+	const evt = document.createEvent('MouseEvents');
+	evt.initEvent('click', false, true);
+	document.body.innerHTML = `<ul><li><a id="result" href="/result">http://localhost:81/results/r?_q=felix</a></li></ul>`;
 	it('Test click event', () => {
-		const evt = document.createEvent('MouseEvents');
-		evt.initEvent('click', false, true);
-		document.body.innerHTML = `<ul><li><a id="result" href="/result">http://localhost:81/results/r?_q=felix</a></li></ul>`;
 
 		const resultButton = document.getElementById('result');
 		resultButton.addEventListener('click', e => {
