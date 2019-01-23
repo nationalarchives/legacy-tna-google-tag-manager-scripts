4362680a2a0ea9ffb5ba56fd1ef6704adc481f68
Punal Chotrani <punalsc@gmail.com>
2019-01-23 11:55:03 +0000

    added polyfill for matches

--- src/imageViewer.js
+++ src/imageViewer.js
@@ -2,19 +2,26 @@ import { controlObj } from './modules/imageViewer/imageControlSchema';
 import { getEventLabel } from './modules/imageViewer/eventLabel';
 import { pushInDataLayer } from './modules/pushInDataLayer';
 
-const handleEvent = (e) => {
-    if (e.target.nodeName === 'A' ||
-        e.target.nodeName === 'SELECT' ||
-        e.target.matches('#thumbPanel img') ||
-        e.target.matches('.control-buttons span')
-    ) {
-        pushInDataLayer(controlObj('Discovery image viewer', 'Discovery', 'Image viewer functions', getEventLabel('title')));
-    }
+if (Element && !Element.prototype.matches) {
+	var proto = Element.prototype;
+	proto.matches = proto.matchesSelector ||
+		proto.mozMatchesSelector || proto.msMatchesSelector ||
+		proto.oMatchesSelector || proto.webkitMatchesSelector;
+}
+
+const handleEvent = (event) => {
+	if (event.target.nodeName === 'A' ||
+		event.target.nodeName === 'SELECT' ||
+		event.target.matches('#thumbPanel img') ||
+		event.target.matches('.control-buttons span')
+	) {
+		pushInDataLayer(controlObj('Discovery image viewer', 'Discovery', 'Image viewer functions', getEventLabel(event, 'title')));
+	}
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    const imageViewer = document.getElementById('imageViewer');
-    if (imageViewer) {
-        imageViewer && imageViewer.addEventListener('click', handleEvent, false);
-    }
+	const imageViewer = document.getElementById('imageViewer');
+	if (imageViewer) {
+		imageViewer && imageViewer.addEventListener('click', handleEvent, false);
+	}
 });
