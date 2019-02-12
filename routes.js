e2b1d9e7db4677fc23584b5033c9886396934912
Punal Chotrani <punalsc@gmail.com>
2019-02-12 13:07:21 +0000

    Update to router

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
 // require express
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
 // create our router object
-var router = express.Router();
+const router = express.Router();
 
 // export our router
 module.exports = router;
