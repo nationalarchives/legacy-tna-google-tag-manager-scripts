1d90553c25354363dbe8150d0733c1c199999b67
JamesWChan <jwchan1993@gmail.com>
2019-02-13 16:04:21 +0000

    Added verifyUrl.js and tests

--- src/modules/accountTracking/verifyURL.js
+++ src/modules/accountTracking/verifyURL.js
@@ -1,2 +1,5 @@
-import { extractMetaTagContent } from "../extractMetaTagContent";
-
+export const verifyUrl = (url, searchTerm, status) => {
+    let contentGroup;
+    (url.includes(searchTerm) && status === 'signed-in') ? contentGroup = 'Account' : contentGroup = null;
+    return contentGroup;
+};
---
+++ test/accountTracking.test.js
@@ -0,0 +1,14 @@
+import { verifyUrl } from '../src/modules/accountTracking/verifyURL';
+import { extractMetaTagContent } from '../src/modules/extractMetaTagContent';
+
+document.body.innerHTML = '<meta name="DCSext.signedin" content="signed-in" />';
+
+describe('Checks that the correct value is returned depending on the URL', () => {
+    let url = 'http://test.discovery.nationalarchives.gov.uk/account/bookmarks';
+    let signedInStatus = extractMetaTagContent('DCSext\\.signedin');
+
+    it('Should return account if the path /account/ is passed, else returns null', () => {
+        expect(verifyUrl(url, '/account/', signedInStatus)).toBe('Account');
+        expect(verifyUrl(url, 'abcdefg', signedInStatus)).toBe(null);
+    });
+});
