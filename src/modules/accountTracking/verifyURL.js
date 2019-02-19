15c5c9207bfde8367195a9ee1c1e65a5789626e6
JamesWChan <jwchan1993@gmail.com>
2019-02-19 10:29:25 +0000

    Added parameter validation and new tests

--- src/modules/accountTracking/verifyURL.js
+++ src/modules/accountTracking/verifyURL.js
@@ -1,5 +1,8 @@
 export const verifyUrl = (url, searchTerm, status) => {
-    let contentGroup;
-    (url.includes(searchTerm) && status === 'signed-in') ? contentGroup = 'Account' : contentGroup = null;
-    return contentGroup;
+    if(typeof url === 'string' && typeof searchTerm === 'string' && typeof status === 'string') {
+        let contentGroup;
+        (url.includes(searchTerm) && status === 'signed-in') ? contentGroup = 'Account' : contentGroup = null;
+        return contentGroup;
+    }
+    return null;
 };
--- test/accountTracking.test.js
+++ test/accountTracking.test.js
@@ -12,3 +12,13 @@ describe('Checks that the correct value is returned depending on the URL', () =>
         expect(verifyUrl(url, 'abcdefg', signedInStatus)).toBe(null);
     });
 });
+
+describe('Checks that the parameters are of type string', () => {
+    let url = 'http://test.discovery.nationalarchives.gov.uk/account/bookmarks';
+    let signedInStatus = extractMetaTagContent('DCSext\\.signedin');
+
+    it('Should return account if the path /account/ is passed, else returns null', () => {
+        expect(verifyUrl(url, '/account/', signedInStatus)).toBe('Account');
+        expect(verifyUrl(123, 'abcdefg', signedInStatus)).toBe(null);
+    });
+});
