5b25725e8562bcd9f141471573bcf3be1886a1ec
JamesWChan <jwchan1993@gmail.com>
2019-02-20 11:53:36 +0000

    Modified tests

--- test/verifyURL.test.js
+++ test/verifyURL.test.js
@@ -1,24 +1,25 @@
 import { verifyUrl } from '../src/modules/discoveryServerSide/verifyURL';
 import { extractMetaTagContent } from '../src/modules/extractMetaTagContent';
 
-document.body.innerHTML = '<meta name="DCSext.signedin" content="signed-in" />';
+document.body.innerHTML =
+    '<meta name="DCSext.signedin" content="signed-in" />' +
+    '<meta name="DCSext.notsignedin" content="Not signed-in" />';
 
-describe('Checks that the correct value is returned depending on the URL', () => {
-    let url = 'http://test.discovery.nationalarchives.gov.uk/account/bookmarks';
-    let signedInStatus = extractMetaTagContent('DCSext\\.signedin');
+let url = 'http://test.discovery.nationalarchives.gov.uk/account/bookmarks';
+let signedIn = extractMetaTagContent('DCSext\\.signedin');
+let notSignedIn = extractMetaTagContent('DCSext\\.notsignedin');
 
-    it('Should return account if the path /account/ is passed, else returns null', () => {
-        expect(verifyUrl(url, '/account/', signedInStatus, 'defaultValue')).toBe('Customer account');
-        expect(verifyUrl(url, 'abcdefg', signedInStatus, 'defaultValue')).toBe('defaultValue');
+describe('Checks that the correct value is returned depending on the URL and login status', () => {
+    it('Should return customer account if the path /account/ is passed and the user is authenicated, else returns default value', () => {
+        expect(verifyUrl(url, '/account/', signedIn, 'defaultValue')).toBe('Customer account');
+        expect(verifyUrl(url, '/account/', notSignedIn, 'defaultValue')).toBe('defaultValue');
+        expect(verifyUrl(url, '/somepath/', notSignedIn, 'defaultValue')).toBe('defaultValue');
     });
 });
 
-describe('Checks that the parameters are of type string', () => {
-    let url = 'http://test.discovery.nationalarchives.gov.uk/account/bookmarks';
-    let signedInStatus = extractMetaTagContent('DCSext\\.signedin');
-
+describe('Checks that all parameters are of type string', () => {
     it('Should return account if the path /account/ is passed, else returns null', () => {
-        expect(verifyUrl(url, '/account/', signedInStatus, 'defaultValue')).toBe('Customer account');
-        expect(verifyUrl(123, 'abcdefg', signedInStatus, 'defaultValue')).toBe(null);
+        expect(verifyUrl(url, '/account/', signedIn, 'defaultValue')).toBe('Customer account');
+        expect(verifyUrl(123, 'abcdefg', signedIn, 'defaultValue')).toBe(null);
     });
 });
