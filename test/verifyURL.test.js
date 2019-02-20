e68cda62d3de09094391ce65d3894a173eba3538
JamesWChan <jwchan1993@gmail.com>
2019-02-20 11:05:55 +0000

    Fixed erroring tests

--- test/verifyURL.test.js
+++ test/verifyURL.test.js
@@ -8,8 +8,8 @@ describe('Checks that the correct value is returned depending on the URL', () =>
     let signedInStatus = extractMetaTagContent('DCSext\\.signedin');
 
     it('Should return account if the path /account/ is passed, else returns null', () => {
-        expect(verifyUrl(url, '/account/', signedInStatus)).toBe('Account');
-        expect(verifyUrl(url, 'abcdefg', signedInStatus)).toBe(null);
+        expect(verifyUrl(url, '/account/', signedInStatus, 'defaultValue')).toBe('Customer account');
+        expect(verifyUrl(url, 'abcdefg', signedInStatus, 'defaultValue')).toBe('defaultValue');
     });
 });
 
@@ -18,7 +18,7 @@ describe('Checks that the parameters are of type string', () => {
     let signedInStatus = extractMetaTagContent('DCSext\\.signedin');
 
     it('Should return account if the path /account/ is passed, else returns null', () => {
-        expect(verifyUrl(url, '/account/', signedInStatus)).toBe('Account');
-        expect(verifyUrl(123, 'abcdefg', signedInStatus)).toBe(null);
+        expect(verifyUrl(url, '/account/', signedInStatus, 'defaultValue')).toBe('Customer account');
+        expect(verifyUrl(123, 'abcdefg', signedInStatus, 'defaultValue')).toBe(null);
     });
 });
