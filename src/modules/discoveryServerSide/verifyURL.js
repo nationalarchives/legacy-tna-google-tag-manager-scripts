493abe8be27ea0257901312f650ebd5ee73903c5
James Chan <jwchan1993@gmail.com>
2019-03-01 10:04:39 +0000

    Refactored verifyURL.js

--- src/modules/discoveryServerSide/verifyURL.js
+++ src/modules/discoveryServerSide/verifyURL.js
@@ -1,8 +1,7 @@
-export const verifyUrl = (url, searchTerm, status, defaultValue) => {
-    if(typeof url === 'string' && typeof searchTerm === 'string' && typeof status === 'string') {
-        let contentGroup;
-        (url.includes(searchTerm) && status === 'signed-in') ? contentGroup = 'Customer account' : contentGroup = defaultValue;
-        return contentGroup;
+export const verifyUrl = (...args) => {
+    const [url, searchTerm, status, defaultValue] = args;
+    if (args.every(arg => typeof arg === 'string')){
+        return url.includes(searchTerm) && status === 'signed-in' ? 'Customer account' : defaultValue;
     }
     return null;
 };
