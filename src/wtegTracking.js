e532476ab8d564c3171f8f9d6fae0dcf29cfaa6e
JamesWChan <jwchan1993@gmail.com>
2019-06-26 16:10:51 +0100

    Add click listener to WTEG expand button
    When the WTEG is expanded, an object is built and sent to the data layer, after which the event listener is removed from the button

--- src/wtegTracking.js
+++ src/wtegTracking.js
@@ -4,9 +4,17 @@ import{pushInDataLayer} from './modules/pushInDataLayer';
 
 // Wait for the DOM to load
 window.addEventListener('DOMContentLoaded', () => {
+	let wtegButton = document.querySelector('#what-to-expect .discoveryPrimaryCallToActionLink');
 
 	// Build promotion object on page load
 	let gtmObj = promoObjFunc('wteg', extractMetaTagContent('Dcsext\\.wteg'), 'Expanding link', 'Below catalogue description', false);
-
 	pushInDataLayer(gtmObj);
+
+	// Add an event listener to the WTEG button to detect when it is expanded
+	// Remove the event listener after the first click
+	wtegButton.addEventListener('click', function handleClick(e) {
+		let gtmObj = promoObjFunc('wteg', extractMetaTagContent('Dcsext\\.wteg'),'Expanding button', 'Below catalogue description', 'promotionClick', true);
+		pushInDataLayer(gtmObj);
+		this.removeEventListener('click', handleClick, false);
+	});
 });
