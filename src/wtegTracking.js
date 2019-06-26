30715e9c728d1a51aa2adda8e0f0f2bbae7a76ef
JamesWChan <jwchan1993@gmail.com>
2019-06-26 16:27:23 +0100

    Add baseWTEGPromoObj
    Add object template to be called on WTEG page load

--- src/wtegTracking.js
+++ src/wtegTracking.js
@@ -2,6 +2,7 @@ import{extractMetaTagContent} from './modules/extractMetaTagContent';
 import{promoObjFunc} from './modules/promoObj';
 import{pushInDataLayer} from './modules/pushInDataLayer';
 import{watermarkCheck} from './modules/discoveryServerSide/watermarkCheck';
+import{baseWTEGObjFunc} from './modules/baseWTEGPromoObj';
 
 // Wait for the DOM to load
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,13 +10,13 @@ window.addEventListener('DOMContentLoaded', () => {
 	let imgViewerLink = document.querySelector('#imageViewerLink');
 
 	// Build promotion object on page load
-	let gtmObj = promoObjFunc('wteg', extractMetaTagContent('Dcsext\\.wteg'), 'Expanding link', 'Below catalogue description', false);
+	let gtmObj = baseWTEGObjFunc('wteg', extractMetaTagContent('Dcsext\\.wteg'), 'Expanding link', 'Below catalogue description');
 	pushInDataLayer(gtmObj);
 
 	// Add an event listener to the WTEG button to detect when it is expanded
 	// Remove the event listener after the first click
 	wtegButton.addEventListener('click', function handleClick(e) {
-		let gtmObj = promoObjFunc('wteg', extractMetaTagContent('Dcsext\\.wteg'),'Expanding button', 'Below catalogue description', 'promotionClick', true);
+		let gtmObj = promoObjFunc('wteg', extractMetaTagContent('Dcsext\\.wteg'),'Expanding button', 'Below catalogue description', 'promotionClick');
 		pushInDataLayer(gtmObj);
 		this.removeEventListener('click', handleClick, false);
 	});
@@ -24,7 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Remove the event listener after the first click
 	if(watermarkCheck('DCSext\\.imgviewer')) {
 		imgViewerLink.addEventListener('click', function handleClick(e) {
-			let gtmObj = promoObjFunc('ivp', extractMetaTagContent('DCSext\\.imgviewer'), 'Image viewer', 'Below record description', 'promotionClick', true);
+			let gtmObj = promoObjFunc('ivp', extractMetaTagContent('DCSext\\.imgviewer'), 'Image viewer', 'Below record description', 'promotionClick');
 			pushInDataLayer(gtmObj);
 			this.removeEventListener('click', handleClick, false);
 		});
