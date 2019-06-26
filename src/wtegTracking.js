952716f3f0a3b4f4bbfa6bb7b894e7430de01d48
JamesWChan <jwchan1993@gmail.com>
2019-06-26 16:12:15 +0100

    Add click listener to image viewer link
    This event listener is only applied when a watermarked image exists on the page. When the image viewer is expanded, an object is built and sent to the data layer, after which the event listener is removed.

--- src/wtegTracking.js
+++ src/wtegTracking.js
@@ -1,10 +1,12 @@
 import{extractMetaTagContent} from './modules/extractMetaTagContent';
 import{promoObjFunc} from './modules/promoObj';
 import{pushInDataLayer} from './modules/pushInDataLayer';
+import{watermarkCheck} from './modules/discoveryServerSide/watermarkCheck';
 
 // Wait for the DOM to load
 window.addEventListener('DOMContentLoaded', () => {
 	let wtegButton = document.querySelector('#what-to-expect .discoveryPrimaryCallToActionLink');
+	let imgViewerLink = document.querySelector('#imageViewerLink');
 
 	// Build promotion object on page load
 	let gtmObj = promoObjFunc('wteg', extractMetaTagContent('Dcsext\\.wteg'), 'Expanding link', 'Below catalogue description', false);
@@ -17,4 +19,14 @@ window.addEventListener('DOMContentLoaded', () => {
 		pushInDataLayer(gtmObj);
 		this.removeEventListener('click', handleClick, false);
 	});
+
+	// If an image is watermarked, add an event listener to the image viewer link to detect when it is expanded
+	// Remove the event listener after the first click
+	if(watermarkCheck('DCSext\\.imgviewer')) {
+		imgViewerLink.addEventListener('click', function handleClick(e) {
+			let gtmObj = promoObjFunc('ivp', extractMetaTagContent('DCSext\\.imgviewer'), 'Image viewer', 'Below record description', 'promotionClick', true);
+			pushInDataLayer(gtmObj);
+			this.removeEventListener('click', handleClick, false);
+		});
+	}
 });
