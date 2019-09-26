738a80bacd1f438070feb9d1122269462e1a16a0
Punal Chotrani <punalsc@gmail.com>
2019-09-26 19:11:19 +0100

    Updaed test and gathering search UI data

--- src/searchUITracking.js
+++ src/searchUITracking.js
@@ -1,28 +1,32 @@
-import { checkWord } from './modules/checkWord';
-
-const fieldValue = domNode => (domNode.value.trim() !== '' ? true : false);
+import { checkEmptyField } from './modules/checkEmptyField';
+//import { pushInDataLayer } from './modules/pushInDataLayer';
 
 window.addEventListener('DOMContentLoaded', () => {
 	if (document.getElementById('discovery-home-page-search')) {
-		const searchFrom = document.querySelector('#discovery-home-page-search'),
-			startDate = document.getElementById('start-date'),
-			error = document.querySelector('#discovery-home-page-search .Headline'),
-			searchFor = document.getElementById('search-for').getAttribute('id');
+		const searchFrom = document.querySelector('#discovery-home-page-search');
+		const searchFor = document.getElementById('search-for');
+		const startDate = document.getElementById('start-date');
+		const endDate = document.getElementById('end-date');
+		const heldby = document.getElementById('held-by');
+		const error = document.querySelector('#discovery-home-page-search .Headline');
+
+		const successObj = () => {
+			return {
+				event: 'Discovery search',
+				eventCategory: 'Discovery homepage search',
+				eventAction: 'Successful search',
+				eventLabel: `(${searchFor.getAttribute('id')} : ${checkEmptyField(
+					searchFor
+				)}) > (${startDate.getAttribute('id')} : ${checkEmptyField(
+					startDate
+				)}) > (${endDate.getAttribute('id')} : ${checkEmptyField(endDate)})`
+			};
+		};
 
 		searchFrom.addEventListener('submit', e => {
-			// if (checkWord('version', 'OnSitePublic')) {
-			// 	if (error.children.length === 0) {
-			// 		// if error div has no children, means no errors
-			// 		console.log({
-			// 			event: 'Discovery search',
-			// 			eventCategory: 'Discovery homepage search',
-			// 			eventAction: 'Successful search',
-			// 			eventLabel: `Fields used: [(field id = ${searchFor})]`
-			// 		});
-			// 	}
-			// }
-
-			console.log(fieldValue(startDate));
+			if (error.children.length === 0) {
+				console.log(successObj());
+			}
 			e.preventDefault();
 		});
 	}
