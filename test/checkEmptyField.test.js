8a50fd51bc4a972682f036ece82701afe038a284
Punal Chotrani <punalsc@gmail.com>
2019-09-26 13:00:50 +0100

    Added test to check for empty fields

---
+++ test/checkEmptyField.test.js
@@ -0,0 +1,22 @@
+import { checkEmptyField } from '../src/modules/checkEmptyField';
+
+// Define the DOM
+document.body.innerHTML = `
+    <form id='formID'>
+        <input type='text' id='inputField-1' value='some value'/>
+        <input type='text' id='inputField-2' value/>
+    </form>
+`;
+
+describe('Check for empty fields', () => {
+	const field1 = document.getElementById('inputField-1');
+	const field2 = document.getElementById('inputField-2');
+	it('Should check for empty fields', () => {
+		expect(checkEmptyField(field2)).toBe(false);
+		expect(checkEmptyField(field1)).toBeTruthy();
+	});
+	it('Should check the type passed as parameter', () => {
+		expect(checkEmptyField('hello')).toBeNull();
+		expect(checkEmptyField(1)).toBeNull();
+	});
+});
