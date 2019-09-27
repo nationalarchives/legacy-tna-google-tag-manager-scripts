b5f252fcc1519f7ce90a059f34bb3d86dbb93d58
Punal Chotrani <punalsc@gmail.com>
2019-09-27 10:53:18 +0100

    Updated more tests to check input type

--- src/modules/checkEmptyField.js
+++ src/modules/checkEmptyField.js
@@ -2,8 +2,8 @@
  *
  * @param {object} domNode
  */
-export const checkEmptyField = domNode => {
-	if (domNode instanceof Element && domNode.type == 'text') {
+export const checkEmptyField = (domNode, inputType) => {
+	if (domNode instanceof Element && domNode.type == inputType) {
 		return domNode.value !== '' ? 'Yes' : 'No';
 	}
 	return null;
