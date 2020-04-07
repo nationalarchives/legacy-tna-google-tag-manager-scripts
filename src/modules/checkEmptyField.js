bd10a19c14a1dcb9bd0a9aaacf69f11da816ee4f
JamesWChan <jwchan1993@gmail.com>
2020-04-07 11:53:04 +0100

    Refactor checkEmptyField to accept additional form inputs

--- src/modules/checkEmptyField.js
+++ src/modules/checkEmptyField.js
@@ -4,7 +4,12 @@
  */
 export const checkEmptyField = (domNode, inputType) => {
     if (domNode instanceof Element && domNode.type == inputType) {
-        return domNode.value !== '' ? 'Yes' : 'No';
+        if(inputType === 'text' || inputType === 'password' || inputType === 'select-one'){
+            return domNode.value !== '' ? 'Yes' : 'No';
+        }
+        else if (inputType === 'checkbox'){
+            return domNode.checked ? 'Yes' : 'No';
+        }
     }
     return null;
 };
