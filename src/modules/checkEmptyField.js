53a00f4365594c44773973e1567ff009c7f903af
JamesWChan <jwchan1993@gmail.com>
2020-04-07 12:45:13 +0100

    Refactor checkEmptyField

--- src/modules/checkEmptyField.js
+++ src/modules/checkEmptyField.js
@@ -4,11 +4,11 @@
  */
 export const checkEmptyField = (domNode, inputType) => {
     if (domNode instanceof Element && domNode.type == inputType) {
-        if(inputType === 'text' || inputType === 'password' || inputType === 'select-one'){
-            return domNode.value !== '' ? 'Yes' : 'No';
-        }
-        else if (inputType === 'checkbox'){
+        if(inputType === 'checkbox'){
             return domNode.checked ? 'Yes' : 'No';
+        }
+        else {
+            return domNode.value !== '' ? 'Yes' : 'No';
         }
     }
     return null;
